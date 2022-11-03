07a9ec61f3acc8ab96b0bf56e16b5c7bf2ae3a25
taaleandersen <19232938+taaleandersen@users.noreply.github.com>
2022-11-03 10:44:30 +0100

    Completed a game of Rock, Paper, Scissors in Javascript. First day on the odin project, trying to learn more in-depth JS.

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+let choices = ['Rock','Paper','Scissors'];
+let playerWins = 0;
+let computerWins = 0;
+let round = 0;
 function getComputerChoice() {
-    let choices = ['Rock','Paper','Scissors'];
     let choice = Math.floor(Math.random() * 3);
     let chosen = choices[choice];
-    console.log(chosen);
+    return chosen;
 }
 function playerChoice(){
-    let choice = prompt('1 = Rock, 2 = Paper, 3 = Scissors');
-    let chosen = choice.val;
-    console.log(chosen);
+    let choice = parseInt(prompt('1 = Rock, 2 = Paper, 3 = Scissors'));
+    let chosen = choices[choice-1];
+    return chosen;
 }
-playerChoice();
-//getComputerChoice();
+
+// let playerSelect = playerChoice();//Commented out for running series
+// let computerSelect = getComputerChoice();
+
+function playRound(playerSelect, computerSelect){
+    round++;
+    if(playerSelect == "Rock" && computerSelect == "Scissors"){
+        playerWins++;
+        console.log("Round" + round + "Player wins the round");
+    }else if(playerSelect == "Scissors" && computerSelect == "Paper"){
+        playerWins++;
+        console.log("Round" + round + "Player wins the round");
+    }else if(playerSelect == "Paper" && computerSelect == "Rock"){
+        playerWins++;
+        console.log("Round" + round + "Player wins the round");
+    }else if(playerSelect === computerSelect){
+        console.log("Round" + round + "Round draw");
+        computerWins++;
+        playerWins++;
+    }else{
+        computerWins++;
+        console.log("Round" + round + "Computer wins the round");
+    }
+    console.log(playerSelect + " " + computerSelect);
+    console.log("P "+playerWins +"C "+ computerWins);
+}
+// playRound(playerSelect, computerSelect);
+function game(){
+    for(let i = 0; i < 5; i++){
+        let playerSelect = playerChoice();
+        let computerSelect = getComputerChoice();
+        playRound(playerSelect, computerSelect);
+    }
+    console.log("Final result:\n Player has: " +playerWins+ " and computer has: " + computerWins);
+    if(computerWins > playerWins){
+        console.log("Computer wins the series");
+    }else if(computerWins === playerWins){
+        console.log("Series ended in draw");
+    }else{
+        console.log("Player wins the series");
+    }
+}
+
+game();
