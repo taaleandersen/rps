3a782d00cf842ae990132ad60d144065832c3d3d
taaleandersen <19232938+taaleandersen@users.noreply.github.com>
2022-11-03 16:10:56 +0100

    Done with UI. There is some bugs in the script, where deciding wich is the winner and playerscores is not showing correctly.

--- script.js
+++ script.js
@@ -1,44 +1,96 @@
-let choices = ['Rock','Paper','Scissors'];
+// !v2.0
+let playerSelect = "";
 let playerWins = 0;
 let computerWins = 0;
 let round = 0;
+let winner = '';
+let player = false;
+//!GAMEFUNCTONS
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     let chosen = choices[choice];
     return chosen;
 }
-function playerChoice(){
-    let choice = parseInt(prompt('1 = Rock, 2 = Paper, 3 = Scissors'));
-    let chosen = choices[choice-1];
-    return chosen;
-}
-
-// let playerSelect = playerChoice();//Commented out for running series
-// let computerSelect = getComputerChoice();
-
 
 function playRound(playerSelect, computerSelect){
     round++;
+    let player = "false";
     if(playerSelect == "Rock" && computerSelect == "Scissors"){
         playerWins++;
+        return player = true;
+        winner == 'Player';
         console.log("Round" + round + "Player wins the round");
     }else if(playerSelect == "Scissors" && computerSelect == "Paper"){
         playerWins++;
+        return player = true;
+        winner == 'Player';
         console.log("Round" + round + "Player wins the round");
     }else if(playerSelect == "Paper" && computerSelect == "Rock"){
         playerWins++;
+        return player = true
+        winner == 'Player';
         console.log("Round" + round + "Player wins the round");
     }else if(playerSelect === computerSelect){
         console.log("Round" + round + "Round draw");
         computerWins++;
         playerWins++;
+        winner = 'DRAW';
     }else{
         computerWins++;
-        console.log("Round" + round + "Computer wins the round");
+        winner == 'Computer';
     }
-    console.log(playerSelect + " " + computerSelect);
+    console.log('player:'+playerSelect + " " +'computer: '+computerSelect);
     console.log("P "+playerWins +"C "+ computerWins);
 }
+
+////BUTTONS
+const btnRock = document.querySelector('#rock'); 
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+
+////CONTAINERS
+const container = document.querySelector('#container');
+const options = container.querySelectorAll('button');
+
+////UI
+const ui = document.querySelector('.ui');
+const result = document.createElement('div');
+ui.appendChild(result);
+
+////SCOREBOARD
+const scoreboard = document.createElement('div');
+const playerScoreHolder = document.createElement('span');
+const computerScoreHolder = document.createElement('span');
+const scoreSeparator = document.createElement('span');
+scoreSeparator.style.cssText = 'margin-left:20px;margin-right:20px;';
+scoreSeparator.innerText = '|';
+playerScoreHolder.innerText = 'PlayerScore: '+playerWins;
+computerScoreHolder.innerText = 'ComputerScore: '+computerWins;
+scoreboard.classList.add('scoreboard');
+scoreboard.appendChild(playerScoreHolder);
+scoreboard.appendChild(scoreSeparator);
+scoreboard.appendChild(computerScoreHolder);
+container.insertBefore(scoreboard, container.firstChild);
+
+options.forEach((button) =>{
+    button.addEventListener('click', (e) => {
+        let computerSelect = getComputerChoice();
+        playerSelect = button["id"];
+        playRound(playerSelect, computerSelect);
+        if(player){
+            playerWins++;
+        }else{
+            computerWins++;
+        }
+        playerSelect = playerSelect.charAt(0).toUpperCase()+playerSelect.substring(1);
+        result.innerHTML = 'Player chose: '+playerSelect+'<br>';
+        result.innerHTML += 'Computer chose: '+computerSelect+'<br>';
+        result.innerHTML += '<center><span style="text-align:center;color: green;">!</span></center>';
+                
+    });
+})
+let choices = ['Rock','Paper','Scissors'];
+
 // playRound(playerSelect, computerSelect);
 function game(){
     
@@ -52,4 +104,4 @@ function game(){
     }
 }
 
-game();
+// game();
