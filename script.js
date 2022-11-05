722bda9835ae0418b633a3170e814ef37e380c4a
taaleandersen <19232938+taaleandersen@users.noreply.github.com>
2022-11-05 15:52:57 +0100

    Changed the game script, fixed bug with choosing winner and not displaying correct results. Finished UI, and added a restart button when game is finished.

--- script.js
+++ script.js
@@ -1,52 +1,16 @@
 // !v2.0
 let playerSelect = "";
+let computerSelect = "";
 let playerWins = 0;
 let computerWins = 0;
 let round = 0;
 let winner = '';
-let player = false;
-//!GAMEFUNCTONS
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    let chosen = choices[choice];
-    return chosen;
-}
-
-function playRound(playerSelect, computerSelect){
-    round++;
-    let player = "false";
-    if(playerSelect == "Rock" && computerSelect == "Scissors"){
-        playerWins++;
-        return player = true;
-        winner == 'Player';
-        console.log("Round" + round + "Player wins the round");
-    }else if(playerSelect == "Scissors" && computerSelect == "Paper"){
-        playerWins++;
-        return player = true;
-        winner == 'Player';
-        console.log("Round" + round + "Player wins the round");
-    }else if(playerSelect == "Paper" && computerSelect == "Rock"){
-        playerWins++;
-        return player = true
-        winner == 'Player';
-        console.log("Round" + round + "Player wins the round");
-    }else if(playerSelect === computerSelect){
-        console.log("Round" + round + "Round draw");
-        computerWins++;
-        playerWins++;
-        winner = 'DRAW';
-    }else{
-        computerWins++;
-        winner == 'Computer';
-    }
-    console.log('player:'+playerSelect + " " +'computer: '+computerSelect);
-    console.log("P "+playerWins +"C "+ computerWins);
-}
+let choices = ['Rock','Paper','Scissors'];
 
 ////BUTTONS
-const btnRock = document.querySelector('#rock'); 
-const btnPaper = document.querySelector('#paper');
-const btnScissors = document.querySelector('#scissors');
+const btnRock = document.querySelector('#Rock'); 
+const btnPaper = document.querySelector('#Paper');
+const btnScissors = document.querySelector('#Scissors');
 
 ////CONTAINERS
 const container = document.querySelector('#container');
@@ -55,6 +19,7 @@ const options = container.querySelectorAll('button');
 ////UI
 const ui = document.querySelector('.ui');
 const result = document.createElement('div');
+const gameMenu = document.querySelector('.game-menu');
 ui.appendChild(result);
 
 ////SCOREBOARD
@@ -72,36 +37,70 @@ scoreboard.appendChild(scoreSeparator);
 scoreboard.appendChild(computerScoreHolder);
 container.insertBefore(scoreboard, container.firstChild);
 
-options.forEach((button) =>{
-    button.addEventListener('click', (e) => {
-        let computerSelect = getComputerChoice();
-        playerSelect = button["id"];
-        playRound(playerSelect, computerSelect);
-        if(player){
+//!GAMEFUNCTONS
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    let chosen = choices[choice];
+    return chosen;
+}
+function resetGame(){
+    window.location = self.location;
+}
+function playRound(playerSelect, computerSelect){
+    round++;
+    console.log("PlayerSelect: " + playerSelect);
+    console.log("computerSelect: " + computerSelect);
+    console.log("PlayerWins: " + playerWins);
+    console.log("computerWins: " + computerWins);
+    console.log("Round: " + round);
+    if(playerWins < 5 && computerWins < 5){
+        if(playerSelect === "Rock" && computerSelect === "Scissors"){
             playerWins++;
+            winner = 'Round winner is Player!';
+        }else if(playerSelect === "Scissors" && computerSelect === "Paper"){
+            playerWins++;
+            winner = 'Round winner is Player!';
+        }else if(playerSelect === "Paper" && computerSelect === "Rock"){
+            playerWins++;
+        }else if(playerSelect === computerSelect){
+            computerWins++;
+            playerWins++;
+            winner = 'DRAW';
         }else{
             computerWins++;
+            winner = 'Round winner is Computer!';
         }
+        playerScoreHolder.innerText = 'PlayerScore: '+playerWins;
+        computerScoreHolder.innerText = 'ComputerScore: '+computerWins;
+        result.innerText = 'Winner is: ' + winner;
+        console.log(winner);
+    }else{
+        gameMenu.innerHTML = "";
+        let restartButton = document.createElement('button');
+        restartButton.classList.add('button');
+        restartButton.innerText = "RESTART";
+        gameMenu.appendChild(restartButton);
+        if(playerWins === 5 || computerWins === 5){
+            if(playerWins > computerWins){
+                result.innerText = 'Winner of 5 rounds is: Player!';
+            }else{
+                result.innerText = 'Winner of 5 rounds is: Computer!';
+            }
+        }
+        restartButton.addEventListener('click', resetGame);
+    }
+    
+}
+
+options.forEach((button) =>{
+    button.addEventListener('click', (e) => {
+        computerSelect = getComputerChoice();
+        playerSelect = button["id"];
+        playRound(playerSelect, computerSelect);
         playerSelect = playerSelect.charAt(0).toUpperCase()+playerSelect.substring(1);
         result.innerHTML = 'Player chose: '+playerSelect+'<br>';
         result.innerHTML += 'Computer chose: '+computerSelect+'<br>';
-        result.innerHTML += '<center><span style="text-align:center;color: green;">!</span></center>';
+        result.innerHTML += `<center><span style="text-align:center;color: green;">Winner is: ${winner}!</span></center>`;
                 
     });
 })
-let choices = ['Rock','Paper','Scissors'];
-
-// playRound(playerSelect, computerSelect);
-function game(){
-    
-    console.log("Final result:\n Player has: " +playerWins+ " and computer has: " + computerWins);
-    if(computerWins > playerWins){
-        console.log("Computer wins the series");
-    }else if(computerWins === playerWins){
-        console.log("Series ended in draw");
-    }else{
-        console.log("Player wins the series");
-    }
-}
-
-// game();
